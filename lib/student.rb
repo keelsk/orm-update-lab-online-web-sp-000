5cc27feb4e8b96d607b7c1dcc868e99a1d5526c1
Kirsten Keels <keelsk@gmail.com>
2020-03-19 17:36:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,10 +55,12 @@ class Student
   end
   
   def self.new_from_db(row)
-    student = self.new(row[1], row[2])
+    student = self.new(row[0], row[1], row[2])
     student
   end
   
   def update
   end
+  
+  def self.find_by_name
 end
