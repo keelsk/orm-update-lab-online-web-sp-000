456163f786a97aee6608c45801214f02600a33d2
Kirsten Keels <keelsk@gmail.com>
2020-03-19 17:20:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,6 +24,14 @@ class Student
     
     DB[:conn].execute(sql)
   end
+  
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
 
 
 end
